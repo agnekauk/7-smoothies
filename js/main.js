75be6bb206e631e2ae2a89249d0f4c642e0023ee
agnekauk <agnenemeikaite@gmail.com>
2022-02-09 22:17:48 +0200

    JS change picture

--- js/main.js
+++ js/main.js
@@ -5,15 +5,21 @@ hamburger.addEventListener('click', () => {
     header.classList.toggle('menu-visible');
 });
 
-let main = document.querySelector("main");
-main.addEventListener('wheel', isKeyPressed);
+// let main = document.querySelector("main");
+// main.addEventListener('wheel', isKeyPressed);
 
-function isKeyPressed(event) {
-    let x = document.getElementById("demo");
-    if (event.ctrlKey) {
-        x.innerHTML = "Jūs ne CTRL spauskit, o rinkitės!!";
-    } else {
-        x.innerHTML = "Dar neišsirinkote?";
-    }
+// function isKeyPressed(event) {
+//     let x = document.getElementById("demo");
+//     if (event.ctrlKey) {
+//         x.innerHTML = "Jūs ne CTRL spauskit, o rinkitės!!";
+//     } else {
+//         x.innerHTML = "Dar neišsirinkote?";
+//     }
+// }
+
+let image = document.querySelector(".gallery");
+image.addEventListener("mouseup", changePicture);
+
+function changePicture(event) {
+    document.getElementsByClassName("drink").src = "../img/pngaaa.png";
 }
-isKeyPressed;
