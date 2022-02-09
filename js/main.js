8b22573f723abbb570e1e52a316f5803f969ff86
agnekauk <agnenemeikaite@gmail.com>
2022-02-09 21:36:50 +0200

    JS on wheel

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ const hamburger = header.querySelector('.hamburger');
 hamburger.addEventListener('click', () => {
     header.classList.toggle('menu-visible');
 });
+
+let main = document.querySelector("main");
+main.addEventListener('wheel', isKeyPressed);
+
+function isKeyPressed(event) {
+    let x = document.getElementById("demo");
+    if (event.ctrlKey) {
+        x.innerHTML = "Jūs ne CTRL spauskit, o rinkitės!!";
+    } else {
+        x.innerHTML = "Dar neišsirinkote?";
+    }
+}
+isKeyPressed;
