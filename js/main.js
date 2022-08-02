664301325aa7930fd08e3c613b95e7176fd220eb
agnekauk <agnenemeikaite@gmail.com>
2022-08-02 13:23:55 +0300

    Minor changes in JS

--- js/main.js
+++ js/main.js
@@ -5,26 +5,20 @@ hamburger.addEventListener('click', () => {
     header.classList.toggle('menu-visible');
 });
 
-// let main = document.querySelector("main");
-// main.addEventListener('wheel', isKeyPressed);
-
-// function isKeyPressed(event) {
-//     let x = document.getElementById("demo");
-//     if (event.ctrlKey) {
-//         x.innerHTML = "Jūs ne CTRL spauskit, o rinkitės!!";
-//     } else {
-//         x.innerHTML = "Dar neišsirinkote?";
-//     }
-// }
-
 let right = document.querySelector(".right");
 right.addEventListener("mouseover", changeColorRight);
+right.addEventListener("mouseout", returnColor);
 let left = document.querySelector(".left");
 left.addEventListener("mouseover", changeColorLeft);
+left.addEventListener("mouseout", returnColor);
 
 function changeColorRight(event) {
     right.style.backgroundColor = "#e95dc6";
 }
 function changeColorLeft(event) {
     left.style.backgroundColor = "#e95dc6";
+}
+function returnColor(event) {
+    right.style.backgroundColor = "#ff5e61";
+    left.style.backgroundColor = "#ff5e61";
 }
